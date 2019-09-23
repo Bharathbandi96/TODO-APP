f9b78e6bd9506d3689ffec9ad11fdcbb66ef5f45
Bharathbandi96 <bharathbandi96@gmail.com>
2019-09-23 12:34:40 +0530

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -25,24 +25,52 @@ import './style.css';
 //     }
 //   }
 // });
-  function add(){
-    var todos = [];
-        document.getElementById('addBtn').addEventListener('click', function () {
-    var list = document.getElementById('toDoList');
-    list.innerHTML = ''; //resetting the list
-          var todoInput = document.getElementById('Cbox').value;
-        todos.push(todoInput)  
+  // function add(){
+  //   var todos = [];
+  //       document.getElementById('addBtn').addEventListener('click', function () {
+  //   var list = document.getElementById('toDoList');
+  //   list.innerHTML = ''; //resetting the list
+  //         var todoInput = document.getElementById('Cbox').value;
+  //       todos.push(todoInput)  
+
+  //       var item = document.createElement("li");
+  //       for (var i = 0; i < todos.length; i++) {
+  //           var listItem = document.createTextNode(todos[i]);
+  //         var li = document.createElement("li");
+  //     li.appendChild(document.createTextNode(todos[i]));
+  //     list.append(li);
+  //       }
+  //     });
+  // }
+
+   var todos = [];
+    document.getElementById('button').addEventListener('click', function () {
+ var list = document.getElementById('toDoList');
+ list.innerHTML = ''; //resetting the list
+      var todoInput = document.getElementById('todoInput').value;
+    todos.push(todoInput)  
 
         var item = document.createElement("li");
-        for (var i = 0; i < todos.length; i++) {
-            var listItem = document.createTextNode(todos[i]);
-          var li = document.createElement("li");
-      li.appendChild(document.createTextNode(todos[i]));
-      list.append(li);
-        }
-      });
-  }
+    for (var i = 0; i < todos.length; i++) {
+        var listItem = document.createTextNode(todos[i]);
+       var li = document.createElement("li");
+  li.appendChild(document.createTextNode(todos[i]));
+  list.append(li);
+    }
 
+    var item = document.createElement("value");
+    for (var i = 0; i < todos.length; i++) {
+        var listItem = document.createTextNode(todos[i]);
+       var value = document.createElement("value");
+  value.appendChild(document.createTextNode(todos[i]));
+  list.append(value);
+    }
+
+  });
+
+    var i = 1;      // COUNTER, FOR CHECKBOX ID.
+
+  
   
 
 
---
+++ scripts/checkbox.js
@@ -0,0 +1,23 @@
+   
+  exports.checkBox  = function createChk(obj) {
+        if (obj.value !== '') {
+
+            var chk = document.createElement('input');  // CREATE CHECK BOX.
+            chk.setAttribute('type', 'checkbox');       // SPECIFY THE TYPE OF ELEMENT.
+            chk.setAttribute('id', 'prodName' + i);     // SET UNIQUE ID.
+            chk.setAttribute('value', obj.value);
+            chk.setAttribute('name', 'products');
+
+            var lbl = document.createElement('label');  // CREATE LABEL.
+            lbl.setAttribute('for', 'prodName' + i);
+
+            // CREATE A TEXT NODE AND APPEND IT TO THE LABEL.
+            lbl.appendChild(document.createTextNode(obj.value));
+
+            // APPEND THE NEWLY CREATED CHECKBOX AND LABEL TO THE <p> ELEMENT.
+            container.appendChild(chk);
+            container.appendChild(lbl);
+
+
+        }
+        }
