cb7631ef6abd9f752921cae95a148a510a842041
Bharathbandi96 <bharathbandi96@gmail.com>
2019-09-23 12:10:58 +0530

    3 files modified

--- index.js
+++ index.js
@@ -2,5 +2,47 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+
+// document.addEventListener('keyup',function(event){
+//   if(event.keyCode == 13){
+//     const toDo = input.value;
+//     if(toDo){
+//       addTodo(toDo,id,false,false);
+//       List.push(
+//         {
+//           name:toDo,
+//           id:id,
+//           done:false,
+//           trash:false
+
+//         }
+//       );
+//       input.value='';
+//       id++;
+//     }
+//   }
+// });
+  function add(){
+    var todos = [];
+        document.getElementById('addBtn').addEventListener('click', function () {
+    var list = document.getElementById('toDoList');
+    list.innerHTML = ''; //resetting the list
+          var todoInput = document.getElementById('Cbox').value;
+        todos.push(todoInput)  
+
+        var item = document.createElement("li");
+        for (var i = 0; i < todos.length; i++) {
+            var listItem = document.createTextNode(todos[i]);
+          var li = document.createElement("li");
+      li.appendChild(document.createTextNode(todos[i]));
+      list.append(li);
+        }
+      });
+  }
+
+  
+
+
