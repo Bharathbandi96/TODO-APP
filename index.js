fceb932312436469f1a0d1246b00aa511e7b03f5
Bharathbandi96 <bharathbandi96@gmail.com>
2019-09-23 18:42:34 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ import './style.css';
 
 
 // Create a "close" button and append it to each list item
-
+// var myNodelist = document.getElementsByTagName("li");
+// var i;
+// for (i = 0; i < myNodelist.length; i++) {
+//   var span = document.createElement("list");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   myNodelist[i].appendChild(span);
+// }
 
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
@@ -12,12 +20,15 @@ for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     var div = this.parentElement;
     div.style.display = "none";
-  }
 
+  }
   }
  
     var todos = [];
     var todos1= [];
+    var closed = 0;
+    var all = 0;
+    var completed = 0;
     //var count=0;
     document.getElementById('button').addEventListener('click', function () {
       var list = document.getElementById('myUL');
@@ -33,6 +44,7 @@ for (i = 0; i < close.length; i++) {
           document.getElementById("myInput").value = "";
           list.append(li);
           todos1.push(todoInput)
+          all = todos1.length;
           todos.pop(todoInput);
         }
       console.log(todos1);
@@ -44,22 +56,25 @@ for (i = 0; i < close.length; i++) {
 
       for (i = 0; i < close.length; i++) {
         close[i].onclick = function() {
+          debugger;
           var div = this.parentElement;
           div.style.display = "none";
+          var z = todos1.splice(0,1)
+          closed = todos1.length;
       }}
     })
-
-
-
 
 // Add a "checked" symbol when clicking on a list item
 var count=0;
 var list = document.querySelector('ul');
+debugger;
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
 }, false);
+
+
 
 
 
