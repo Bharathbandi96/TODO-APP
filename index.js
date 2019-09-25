56d554914a1db1cbbb1def23cd70d974668b3357
Bharathbandi96 <bharathbandi96@gmail.com>
2019-09-25 17:45:42 +0530

    2 files modified

--- index.js
+++ index.js
@@ -15,60 +15,25 @@ var AllText = 'Total number of tasks in your todo list : '
 var PendingText = 'Pending tasks in your todo list : '
 
 
-// document.getElementById('button').addEventListener('click', function () {
-//     var list = document.getElementById('displayArea');
-//     //list.innerHTML = ''; //resetting the list
-//     var todoInput = document.getElementById('myInput').value;
-//     todos.push(todoInput)
-//     for (var i = 0; i < todos.length; i++) {
-//       var listItem = document.createTextNode(todos[i]);
-//       var li = document.createElement("li");
-//       li.setAttribute("id",setAttr)
-//       li.appendChild(listItem);
-//       document.getElementById("myInput").value = "";
-//       list.append(li);
-//       allTodos.push(todoInput)
-//       closed.push(todoInput)
-//       todos.pop(todoInput);
-//     }
-//     setAttr++;
-
-//     var span = document.createElement("list");
-//     var txt = document.createTextNode("\u00D7");
-//     span.className = "close";
-//     span.appendChild(txt);
-//     li.appendChild(span);
-
-//     for (var i = 0; i < close.length; i++) {
-//       close[i].onclick = function(i) {
-//         var div = this.parentElement;
-//         getAttr = div.getAttribute('id');
-//         deleted.unshift(closed[getAttr]);
-//         console.log(deleted);
-//         div.style.display = "none";
-//         div.className = '';
-//         allTodos.pop();
-//       }
-//     }
-//     })
-
 document.getElementById('button').addEventListener('click', newElement );
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
+  var textNode = document.createTextNode(inputValue);
+  li.appendChild(textNode);
   if (inputValue === '') {
     alert("You must write something!");
   } else {
     document.getElementById("displayArea").appendChild(li);
+    todos.push(inputValue);
+    console.log(todos);
   }
   document.getElementById("myInput").value = "";
 
-  var span = document.createElement("button");
-  var txt = document.createTextNode("\u00D7");
+  var span = document.createElement("SPAN");
+  var cancel = document.createTextNode("\u00D7");
   span.className = "close";
-  span.appendChild(txt);
+  span.appendChild(cancel);
   li.appendChild(span);
 }
 
@@ -78,6 +43,7 @@ function deleteElement(){
   for (var i = 0; i < close.length; i++) {
       close[i].onclick = function() {
         var div = this.parentElement;
+        console.log(todos.splice(i,1));
         div.remove();
       }
     }
