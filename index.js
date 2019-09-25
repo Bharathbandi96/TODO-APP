f64ca36f9633ac6b3465fcb5f165281849a805b9
Bharathbandi96 <bharathbandi96@gmail.com>
2019-09-25 17:10:57 +0530

    1 file modified

--- index.js
+++ index.js
@@ -10,43 +10,68 @@ var all = 0;
 var setAttr = 0;
 var getAttr = 0;
 
-document.getElementById('button').addEventListener('click', function () {
-    var list = document.getElementById('displayArea');
-    //list.innerHTML = ''; //resetting the list
-    var todoInput = document.getElementById('myInput').value;
-    todos.push(todoInput)
-    for (var i = 0; i < todos.length; i++) {
-      var listItem = document.createTextNode(todos[i]);
-      var li = document.createElement("li");
-      li.setAttribute("id",setAttr)
-      li.appendChild(listItem);
-      document.getElementById("myInput").value = "";
-      list.append(li);
-      allTodos.push(todoInput)
-      closed.push(todoInput)
-      todos.pop(todoInput);
+// document.getElementById('button').addEventListener('click', function () {
+//     var list = document.getElementById('displayArea');
+//     //list.innerHTML = ''; //resetting the list
+//     var todoInput = document.getElementById('myInput').value;
+//     todos.push(todoInput)
+//     for (var i = 0; i < todos.length; i++) {
+//       var listItem = document.createTextNode(todos[i]);
+//       var li = document.createElement("li");
+//       li.setAttribute("id",setAttr)
+//       li.appendChild(listItem);
+//       document.getElementById("myInput").value = "";
+//       list.append(li);
+//       allTodos.push(todoInput)
+//       closed.push(todoInput)
+//       todos.pop(todoInput);
+//     }
+//     setAttr++;
+
+//     var span = document.createElement("list");
+//     var txt = document.createTextNode("\u00D7");
+//     span.className = "close";
+//     span.appendChild(txt);
+//     li.appendChild(span);
+
+//     for (var i = 0; i < close.length; i++) {
+//       close[i].onclick = function(i) {
+//         var div = this.parentElement;
+//         getAttr = div.getAttribute('id');
+//         deleted.unshift(closed[getAttr]);
+//         console.log(deleted);
+//         div.style.display = "none";
+//         div.className = '';
+//         allTodos.pop();
+//       }
+//     }
+//     })
+
+document.getElementById('button').addEventListener('click', newElement );function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("displayArea").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (var i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.remove();
     }
-    setAttr++;
-
-    var span = document.createElement("list");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-
-    for (var i = 0; i < close.length; i++) {
-      close[i].onclick = function(i) {
-        var div = this.parentElement;
-        getAttr = div.getAttribute('id');
-        deleted.unshift(closed[getAttr]);
-        console.log(deleted);
-        div.style.display = "none";
-        div.className = '';
-        allTodos.pop();
-      }
-    }
-    })
-
+  }
+}
     var list = document.querySelector('ul');
     list.addEventListener('click', function(ev) {
       if (ev.target.tagName === 'LI') 
